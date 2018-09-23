692afc181ef08dcdec52f8d97af213d88fe6c048
dantifer <dantifer@gmail.com>
2018-09-23 23:20:59 +0800

    update

--- update.sh
+++ update.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-git status
+echo "hexo generate"
 hexo generate
+echo "git status"
+git status
+echo "git add ."
 git add .
+echo """git commit -a -m "update""""
 git commit -a -m "update"
+echo "git push"
 git push
